a20f8dbaa75c86a387516349cd2a43eded4b2ed3
ZF <zfarik@spartaglobal.com>
2022-03-08 14:00:34 +0000

    added bunch of stuff

---
+++ app.py
@@ -0,0 +1,36 @@
+# install flask with pip install flask
+from config_manager import itjobswatch_home_page_url
+from src.itjobswatch_html_readers.itjobswatch_home_page_top_30 import ItJobsWatchHomePageTop30
+from src.csv_generators.top_30_csv_generator import Top30CSVGenerator
+import os
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+# creating an app instance
+
+# use a decorator @ to define the route for our web page
+@app.route("/")
+# setting up a default page
+def index():
+    return render_template('index.html')
+
+
+@app.route("/team/")
+def team():
+    return render_template('team.html')
+
+@app.route("/tools/")
+def tools():
+    return render_template("tools.html")
+
+@app.route("/tables")
+def table():
+    top_30 = ItJobsWatchHomePageTop30(itjobswatch_home_page_url())
+    Top30CSVGenerator().generate_top_30_csv(top_30.get_top_30_table_elements_into_array(), os.path.expanduser('top30/'), 'ItJobsWatchTop30.csv', top_30.get_table_headers_array())
+    filename = 'top30/ItJobsWatchTop30.csv'
+    data = pd.read_csv(filename)
+    # data.columns=["6 Months up to 8 March 2022", "YoY Change","Median Salary","Median Salary YoY Change","Total Unique Permanent Jobs During Period","Live Jobs"]
+    return render_template('tables.html', tables=[data.to_html()], titles=[''])
+    
+
--- src/csv_generators/top_30_csv_generator.py
+++ src/csv_generators/top_30_csv_generator.py
@@ -1,5 +1,6 @@
 import csv
 import os
+from io import StringIO
 
 class Top30CSVGenerator:
 
@@ -10,6 +11,16 @@ class Top30CSVGenerator:
                 writer.writerow(headers_array)
             writer.writerows(top_30_array)
 
+# class Top30CSVGeneratorString:
+
+#     def generate_top_30_csv(self, top_30_array, csv_file_location=os.path.expanduser('~/Downloads/'), file_name='ItJobsWatchTop30.csv', headers_array=None):
+#         with open(csv_file_location + file_name, 'w+', newline="") as top30csv:
+#             writer = csv.writer(top30csv)
+#             if headers_array is not None:
+#                 writer.writerow(headers_array)
+#             writer.writerows(top_30_array)
+#         new_csvfile = StringIO.StringIO()
+#         wr = csv.writer(new_csvfile, )
 
 if __name__ == '__main__':
     print()
