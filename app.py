120b134485258db9348a1e6a769a414e4c377260
ZF <zfarik@spartaglobal.com>
2022-03-08 18:53:44 +0000

    fixed decoding bug and site layout

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from src.csv_generators.top_30_csv_generator import Top30CSVGenerator
 import os
 from flask import Flask, render_template
 import pandas as pd
+import time
 
 app = Flask(__name__)
 # creating an app instance
@@ -14,8 +15,9 @@ app = Flask(__name__)
 # setting up a default page
 def index():
     top_30 = ItJobsWatchHomePageTop30(itjobswatch_home_page_url())
-    Top30CSVGenerator().generate_top_30_csv(ItJobsWatchHomePageTop30(itjobswatch_home_page_url()).get_top_30_table_elements_into_array(), os.path.expanduser('top30/'), 'ItJobsWatchTop30.csv') #, top_30.get_table_headers_array())
-    data = pd.read_csv('top30/ItJobsWatchTop30.csv', header=None, encoding= 'unicode_escape')
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    Top30CSVGenerator().generate_top_30_csv(ItJobsWatchHomePageTop30(itjobswatch_home_page_url()).get_top_30_table_elements_into_array(), os.path.expanduser('csvfiles/'), timestr + ".csv") #, top_30.get_table_headers_array())
+    data = pd.read_csv('csvfiles/' + timestr + '.csv', header=None, encoding='utf_8')
     data.columns=["", "Job Ranking Over Last 6 Months (up to 8 March 2022)", "YoY (Year-over-Year) Change","Median Salary","Median Salary YoY (Year-over-Year) Change","Total Unique Permanent Jobs During 6 Month Period","Current Live Jobs"]
     return render_template('index.html', myData=[data.values], columns=data.columns)
     
@@ -29,13 +31,4 @@ def team():
 @app.route("/tools/")
 def tools():
     return render_template("tools.html")
-
-# @app.route("/tables")
-# def table():
-#     top_30 = ItJobsWatchHomePageTop30(itjobswatch_home_page_url())
-#     Top30CSVGenerator().generate_top_30_csv(ItJobsWatchHomePageTop30(itjobswatch_home_page_url()).get_top_30_table_elements_into_array(), os.path.expanduser('top30/'), 'ItJobsWatchTop30.csv') #, top_30.get_table_headers_array())
-#     data = pd.read_csv('top30/ItJobsWatchTop30.csv', header=None)
-#     data.columns=["", "Job Ranking Over Last 6 Months (up to 8 March 2022)", "YoY (Year-over-Year) Change","Median Salary","Median Salary YoY (Year-over-Year) Change","Total Unique Permanent Jobs During 6 Month Period","Current Live Jobs"]
-#     return render_template('tables.html', myData=[data.values], columns=data.columns)
-    
 
