7ee032d4e7feecd07598f6a9b122e8765b4794ec
ZF <zfarik@spartaglobal.com>
2022-03-08 17:22:12 +0000

    fixed decoding error

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 def index():
     top_30 = ItJobsWatchHomePageTop30(itjobswatch_home_page_url())
     Top30CSVGenerator().generate_top_30_csv(ItJobsWatchHomePageTop30(itjobswatch_home_page_url()).get_top_30_table_elements_into_array(), os.path.expanduser('top30/'), 'ItJobsWatchTop30.csv') #, top_30.get_table_headers_array())
-    data = pd.read_csv('top30/ItJobsWatchTop30.csv', header=None)
+    data = pd.read_csv('top30/ItJobsWatchTop30.csv', header=None, encoding= 'unicode_escape')
     data.columns=["", "Job Ranking Over Last 6 Months (up to 8 March 2022)", "YoY (Year-over-Year) Change","Median Salary","Median Salary YoY (Year-over-Year) Change","Total Unique Permanent Jobs During 6 Month Period","Current Live Jobs"]
     return render_template('index.html', myData=[data.values], columns=data.columns)
     
--- src/csv_generators/top_30_csv_generator.py
+++ src/csv_generators/top_30_csv_generator.py
@@ -11,16 +11,5 @@ class Top30CSVGenerator:
                 writer.writerow(headers_array)
             writer.writerows(top_30_array)
 
-# class Top30CSVGeneratorString:
-
-#     def generate_top_30_csv(self, top_30_array, csv_file_location=os.path.expanduser('~/Downloads/'), file_name='ItJobsWatchTop30.csv', headers_array=None):
-#         with open(csv_file_location + file_name, 'w+', newline="") as top30csv:
-#             writer = csv.writer(top30csv)
-#             if headers_array is not None:
-#                 writer.writerow(headers_array)
-#             writer.writerows(top_30_array)
-#         new_csvfile = StringIO.StringIO()
-#         wr = csv.writer(new_csvfile, )
-
 if __name__ == '__main__':
     print()
